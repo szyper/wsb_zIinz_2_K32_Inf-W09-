fac7e407542a15d06f0a2d03e866ab33162fa5f3
szyper <zskgit@gmail.com>
2023-04-02 08:54:09 +0200

    php

---
+++ 3_db/3_db_table_delete.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="../css/table.css">
+	<title>Użytkownicy</title>
+</head>
+<body>
+	<h4>Użytkownicy</h4>
+	<table>
+		<tr>
+			<th>Imię</th>
+			<th>Nazwisko</th>
+			<th>Data urodzenia</th>
+			<th>Miasto</th>
+			<th>Województwo</th>
+			<th>Państwo</th>
+		</tr>
+
+	<?php
+		require_once "../scripts/connect.php";
+	$sql = "SELECT `u`.`id` `userId`,`u`.`firstName` `imie`, `u`.`lastName`, `u`.`birthday`, `c`.`city`, `s`.`state`, `co`.`country` FROM `users` u JOIN `cities` c ON `u`.`city_id`=`c`.`id` JOIN `states` s ON `c`.`state_id`=`s`.`id` JOIN `countries` co ON `s`.`country_id`=`co`.`id`;";
+		$result = $conn->query($sql);
+		while($user = $result->fetch_assoc()){
+			echo <<< TABLEUSERS
+				<tr>
+<!--					<td>$user[userId]</td>-->
+					<td>$user[imie]</td>
+					<td>$user[lastName]</td>
+					<td>$user[birthday]</td>
+					<td>$user[city]</td>
+					<td>$user[state]</td>
+					<td>$user[country]</td>
+					<td><a href="../scripts/delete_user.php?userIdDelete=$user[userId]">Usuń</a></td>
+				</tr>
+TABLEUSERS;
+		}
+		echo "</table>";
+	?>
+
+</body>
+</html>
--- scripts/connect.php
+++ scripts/connect.php
@@ -1,4 +1,4 @@
 <?php
-	$conn = new mysqli("localhost", "janusz_k32", "]kG40!7(RQ[yr4pm", "wsb_ziinz_2_k32_inf (w09)");
+	$conn = new mysqli("localhost", "root", "", "wsb_ziinz_2_k32_inf (w09)");
 	//echo "db";
 	//echo $conn->connect_errno;
